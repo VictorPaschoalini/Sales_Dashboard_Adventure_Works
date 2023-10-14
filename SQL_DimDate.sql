ccd01eb1276652be1ba774a4b34150bda1c3e80a
Victor Paschoalini <victor.paschoalini.gouveia@gmail.com>
2023-10-14 00:08:18 -0300

    Add files via upload

---
+++ SQL_DimDate.sql
@@ -0,0 +1,24 @@
+-- Cleansed DimDate --
+SELECT 
+  [DateKey], 
+  [FullDateAlternateKey] AS Date, 
+  -- [DayNumberOfWeek], 
+  [EnglishDayNameOfWeek] AS Day, 
+  -- [SpanishDayNameOfWeek], 
+  -- [FrenchDayNameOfWeek], 
+  -- [DayNumberOfMonth], 
+  -- [DayNumberOfYear], 
+  [WeekNumberOfYear] AS WeekNr, 
+  [EnglishMonthName] AS Month,
+  LEFT([EnglishMonthName],3) AS MonthShort,
+  -- [SpanishMonthName], 
+  -- [FrenchMonthName], 
+  [MonthNumberOfYear] AS MonthNo, 
+  [CalendarQuarter] AS Quarter, 
+  [CalendarYear] AS Year
+  -- [CalendarSemester], 
+  -- [FiscalQuarter], 
+  -- [FiscalYear], 
+  -- [FiscalSemester]
+  FROM
+     [AdventureWorksDW2022].[dbo].[DimDate]
---
+++ SQL_FactInternetSales_MainTable.sql
@@ -0,0 +1,31 @@
+-- Cleansed FACT_InternetSales Table --
+SELECT 
+  [ProductKey], 
+  [OrderDateKey], 
+  [DueDateKey], 
+  [ShipDateKey], 
+  [CustomerKey], 
+  --  ,[PromotionKey]
+  --  ,[CurrencyKey]
+  --  ,[SalesTerritoryKey]
+  [SalesOrderNumber], 
+  --  [SalesOrderLineNumber], 
+  --  ,[RevisionNumber]
+  --  ,[OrderQuantity], 
+  --  ,[UnitPrice], 
+  --  ,[ExtendedAmount]
+  --  ,[UnitPriceDiscountPct]
+  --  ,[DiscountAmount] 
+  --  ,[ProductStandardCost]
+  --  ,[TotalProductCost] 
+  [SalesAmount] --  ,[TaxAmt]
+  --  ,[Freight]
+  --  ,[CarrierTrackingNumber] 
+  --  ,[CustomerPONumber] 
+  --  ,[OrderDate] 
+  --  ,[DueDate] 
+  --  ,[ShipDate] 
+FROM 
+  [AdventureWorksDW2022].[dbo].[FactInternetSales]
+ORDER BY
+  OrderDateKey ASC
